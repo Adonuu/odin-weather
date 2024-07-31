bf78bb05de809083323db158f2279d0ee3597aa8
Adam Young <adamyoung1414@gmail.com>
2024-07-31 19:16:20 -0400

    added search input to html, still need function to make it work moved initial rendering of page to function to make it easier once search function is implemented

--- src/index.js
+++ src/index.js
@@ -10,14 +10,29 @@ const cityLongitude = document.querySelector('#cityLongitude');
 
 let localWeather = await getWeather("Cincinnati");
 let weatherClass = new Weather(localWeather);
-let currentDayDiv = renderDay(weatherClass.getCurrentDay());
-currentDay.appendChild(currentDayDiv);
-cityName.innerHTML = weatherClass.getCurrentCity();
-cityLatitude.innerHTML = 'Latitude: ' + weatherClass.getCityLatitude();
-cityLongitude.innerHTML = 'Longitude: ' + weatherClass.getCityLongitude();
 
-for (let i = 0; i < 6; i++) {
-    let nextWeekDiv = renderDay(weatherClass.getNextWeek()[i]);
-    let nextWeekDay = document.querySelector('#nextWeek' + i);
-    nextWeekDay.appendChild(nextWeekDiv);
+renderPage(weatherClass);
+
+function renderPage(weatherClass) {
+    // reset inner html to remove old data
+    currentDay.innerHTML = '';
+    cityName.innerHTML = '';
+    cityLatitude.innerHTML = '';
+    cityLongitude.innerHTML = '';
+    for (let i = 0; i < 6; i++) {
+        let nextWeekDay = document.querySelector('#nextWeek' + i);
+        nextWeekDay.innerHTML = '';
+    }
+    let currentDayDiv = renderDay(weatherClass.getCurrentDay());
+    currentDay.appendChild(currentDayDiv);
+    cityName.innerHTML = weatherClass.getCurrentCity();
+    cityLatitude.innerHTML = 'Latitude: ' + weatherClass.getCityLatitude();
+    cityLongitude.innerHTML = 'Longitude: ' + weatherClass.getCityLongitude();
+    
+    for (let i = 0; i < 6; i++) {
+        let nextWeekDiv = renderDay(weatherClass.getNextWeek()[i]);
+        let nextWeekDay = document.querySelector('#nextWeek' + i);
+        nextWeekDay.appendChild(nextWeekDiv);
+    }
 }
+
